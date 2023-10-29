06adb537b9db568de4f758e5b6ac2cfde37ebc7f
Leonardo <leodalponte1@gmail.com>
2023-10-28 23:46:28 -0300

    Cadastrando imagem no Produto

--- src/main/java/morango_esmeralda/controllers/ProdutoController.java
+++ src/main/java/morango_esmeralda/controllers/ProdutoController.java
@@ -1,5 +1,6 @@
 package morango_esmeralda.controllers;
 
+import ch.qos.logback.core.model.Model;
 import morango_esmeralda.domain.Produto;
 import morango_esmeralda.dtos.requests.ProdutoRequestDTO;
 import morango_esmeralda.dtos.responses.ProdutoResponseDTO;
@@ -7,12 +8,22 @@ import morango_esmeralda.repository.ProdutoRepository;
 import morango_esmeralda.repository.UsuarioRepository;
 import morango_esmeralda.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 
 @RestController
 @RequestMapping("api/produtos")
 public class ProdutoController {
+
+
     @Autowired
     private ProdutoService produtoService;
 
@@ -22,8 +33,17 @@ public class ProdutoController {
         return produtoService.buscarPeloId(idProduto);
     }
 
-    @PostMapping("")
+
+    @PostMapping("/salvar")
     public ProdutoResponseDTO salvar(@RequestBody ProdutoRequestDTO produtoRequestDTO) {
         return produtoService.salvar(produtoRequestDTO);
+    }
+
+    @PostMapping("/{id}/image")
+    public ResponseEntity<Void> salvarImagem(
+            @PathVariable("id")  Integer id,
+            @RequestParam("fileImage") MultipartFile imagem) throws IOException {
+        produtoService.salvarImagem(imagem,id);
+        return ResponseEntity.ok().build();
     }
 }
